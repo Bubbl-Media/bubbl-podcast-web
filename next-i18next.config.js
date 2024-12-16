4109dd7604f66e3562b2cc0ea708b8812e943f24
KB <kaz@reddirtmining.io>
2024-12-16 15:37:37 -0600

    deployment work

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,11 +1,17 @@
-module.exports = {
-  i18n: {
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import { locales } from './src/lib/utility/locales';  // Import your locales
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: locales,
+    fallbackLng: 'en',
     defaultLocale: 'en',
-    // Remember to update src/lib/utility/locales.ts
-    // and src/lib/utility/date.ts as well
-    locales: ['da', 'de', 'el', 'en', 'es', 'fr', 'it', 'lt', 'nb-NO', 'nl', 'pt', 'pt-BR', 'ru', 'sv', 'tr']
-  },
-  react: {
-    useSuspense: false
-  }
-}
+    supportedLngs: ['da', 'de', 'el', 'en', 'es', 'fr', 'it', 'lt', 'nb-NO', 'nl', 'pt', 'pt-BR', 'ru', 'sv', 'tr'],
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;

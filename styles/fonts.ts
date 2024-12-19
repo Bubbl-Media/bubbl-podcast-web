4ad52ee706b96b51c7f06967e135d96c7021c77f
KB <kaz@reddirtmining.io>
2024-12-19 10:53:58 -0600

    clean up tile stlying

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -1,1 +1,5 @@
-import '@fontsource/open-sans'
+import '@fontsource/open-sans/300.css'  // Light
+import '@fontsource/open-sans/400.css'  // Regular
+import '@fontsource/open-sans/500.css'  // Medium
+import '@fontsource/open-sans/600.css'  // Semi-bold
+import '@fontsource/open-sans/700.css'  // Bold
